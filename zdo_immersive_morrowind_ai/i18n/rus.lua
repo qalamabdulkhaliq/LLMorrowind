93e07e8d926f3bcfbca2212d042883cad7491a03
qalamabdulkhaliq <xxpraxiaxx@gmail.com>
2026-03-04 15:28:55 -0600

    Delete zdo_immersive_morrowind_ai directory

--- zdo_immersive_morrowind_ai/i18n/rus.lua
+++
@@ -1,5 +0,0 @@
-return {
-    ["listening"] = "(слушаю...) ",
-    ["connecting"] = "Подключение к серверу Immersive Morrowind... (игра может подвиснуть на пару секунд)",
-    ["connected"] = "Подключен к серверу Immersive Morrowind"
-}
